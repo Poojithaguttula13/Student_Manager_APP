73037c5447a2356786d65f479f464687a2618f3b
Poojithaguttula13 <poojithaguttula1306@gmail.com>
2025-08-04 18:44:31 +0530

    Test cases were done

--- src/features/students/StudentPage.jsx
+++ src/features/students/StudentPage.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Box, Typography, Paper, Stack, MenuItem } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { useDispatch, useSelector } from 'react-redux';
-// import { createStudent, getStudents, removeStudent } from '../features/studentSlice';
 import { useNavigate } from 'react-router-dom';
 import { createStudent, getStudents, removeStudent } from './StudentsSlice';
 
--- src/jest.config.js
+++ src/jest.config.js
@@ -1,5 +1,16 @@
-export default {
-    transform: {},
-    moduleFileExtensions: ['js', 'json'],
-  };
-  
+module.exports = {
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(axios)/)', // tell Jest to process axios
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // for CSS imports
+    '\\.(jpg|jpeg|png|svg)$': '<rootDir>/__mocks__/fileMock.js' // optional
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+};
