5a601b11c86338bacba8896b632eb535709632fe
Poojithaguttula13 <poojithaguttula1306@gmail.com>
2025-08-04 20:59:21 +0530

    new changes were added

--- src/jest.config.js
+++ src/jest.config.js
@@ -12,5 +12,5 @@ module.exports = {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // for CSS imports
     '\\.(jpg|jpeg|png|svg)$': '<rootDir>/__mocks__/fileMock.js' // optional
   },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleFileExtensions: ['js', 'jsx'],
 };
