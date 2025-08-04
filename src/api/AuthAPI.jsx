43bf758d2294b6117f8d8b57cfd8b681488810cf
Poojithaguttula13 <poojithaguttula1306@gmail.com>
2025-08-04 18:55:42 +0530

    Delete src directory

--- src/api/AuthAPI.jsx
+++
@@ -1,7 +0,0 @@
-import db from "../data/db.json"
-export const validateCredentials = (username, password) => {
- 
-    return username === db.credentials.username && password === db.credentials.password
-      ? db.credentials.token
-      : null;
-  };
